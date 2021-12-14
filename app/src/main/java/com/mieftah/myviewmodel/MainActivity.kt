34b5bef8acdcb25f7e68be455bbb50b80fde8019
Miftahul93 <miftahululum693@gmail.com>
2021-12-14 20:58:41 +0700

    Okk

--- app/src/main/java/com/mieftah/myviewmodel/MainActivity.kt
+++ app/src/main/java/com/mieftah/myviewmodel/MainActivity.kt
@@ -2,10 +2,54 @@ package com.mieftah.myviewmodel
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.lifecycle.ViewModelProvider
+import com.mieftah.myviewmodel.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var activityMainBinding: ActivityMainBinding
+    //private lateinit var viewModel: MainViewModel
+    //menyingkat iniziazi viewmodel
+    private val viewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding.root)
+
+        // dizingkat jadi dihilangkan iniziazinya
+        //viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+
+        displayResult()
+
+        activityMainBinding.btnCalculate.setOnClickListener {
+            val length = activityMainBinding.edtLength.text.toString()
+            val width = activityMainBinding.edtWidth.text.toString()
+            val height = activityMainBinding.edtHeight.text.toString()
+
+            when {
+                width.isEmpty() -> {
+                    activityMainBinding.edtWidth.error = "Masih Kosong"
+                }
+
+                height.isEmpty() -> {
+                    activityMainBinding.edtHeight.error = "Masih Kosong"
+                }
+
+                length.isEmpty() -> {
+                    activityMainBinding.edtLength.error = "Masih Kosong"
+                }
+
+                else -> {
+                    viewModel.calculate(width, height, length)
+                    displayResult()
+                }
+            }
+        }
+    }
+
+    private fun displayResult() {
+        activityMainBinding.tvResult.text = viewModel.result.toString()
     }
 }
